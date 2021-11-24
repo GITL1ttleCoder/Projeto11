dc62008cd1f206dac0de4ddf874c2251c1b42652
GITL1ttleCoder <94476931+GITL1ttleCoder@users.noreply.github.com>
2021-11-24 16:44:10 -0300

    Add files via upload

---
+++ Pro-c11-sa1-template-main/sketch.js
@@ -0,0 +1,29 @@
+var pesos = [35,38,42,45,43,34,36,41,48,32];
+var soma = 0;
+
+
+
+for (var indice = 0;indice<10;indice = indice+1){
+
+soma = soma + pesos[indice]
+
+
+
+
+}
+
+var media = soma/10;
+console.log(media);
+
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
